f9ad2b761f164477850cbda05d6da68c16c33769
Luan Tonin Galvan <ltoningalvan@gmail.com>
2024-07-25 02:38:10 -0300

    feat: utiliza imagens de última geração

--- src/app/(home)/(sections)/MeetOurTeam/MeetOurTeam.view.tsx
+++ src/app/(home)/(sections)/MeetOurTeam/MeetOurTeam.view.tsx
@@ -11,25 +11,25 @@ export function MeetOurTeamSection() {
 
         <div className={styles.teamMembersGrid}>
           <Image
-            src="/team-member-1.png"
+            src="/team-member-1.webp"
             alt="Foto de uma colaboradora mulher"
             width={320}
             height={183}
           />
           <Image
-            src="/team-member-2.png"
+            src="/team-member-2.webp"
             alt="Foto de uma colaboradora mulher"
             width={320}
             height={183}
           />
           <Image
-            src="/team-member-3.png"
+            src="/team-member-3.webp"
             alt="Foto de uma colaboradora mulher"
             width={320}
             height={183}
           />
           <Image
-            src="/team-member-4.png"
+            src="/team-member-4.webp"
             alt="Foto de uma colaboradora mulher"
             width={320}
             height={183}
